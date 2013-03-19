ebaf87ca0c2677ed89fd40a1f33701dc6e443182
Thuc Huynh <thuchuynh@greystonevn.com>
2013-03-19 11:06:05 +0700

    Update SessionManager, DBSessionManager, FileSessionManager, SessionModel

--- ww.core/DBSessionManager.php
+++ ww.core/DBSessionManager.php
@@ -29,10 +29,34 @@ class DBSessionManager extends ISessionManager {
 
     public function read($id) {
         
+        $newid = mysql_real_escape_string($id);
+        $time = time();
+        $fields = array("session_data");
+        $where = array();
+        $where['key'] = "session_id";
+        $where['value'] = $newid;
+        
+        $and  =  array();
+        $and[0]["key"] = "expires";
+        $and[0]["value"]= $time;
+        $and[0]['math'] = " >";
+        
+        return $this->find($fields,$where,$and,null,null);
+        
+        
     }
 
     public function write($id, $data) {
         
+         $time = time() + $this->life_time;
+         $newid = mysql_real_escape_string($id);
+         $newdata = mysql_real_escape_string($data);
+         $sessionModel = new SessionModel();
+         $sessionModel->setSession_id($newid);
+         $sessionModel->setSession_data($newdata);
+         $sessionModel->setExpires($time);
+         $sessionModel->save();
+         
     }
 
 }
--- ww.model/SessionModel.php
+++ ww.model/SessionModel.php
@@ -22,8 +22,32 @@ class SessionModel extends BaseModel{
      */
     private $expires;
     
-    
-    
+    public function getSession_id() {
+        
+        return $this->session_id;
+    }
+
+    public function setSession_id($session_id) {
+        $this->session_id = $session_id;
+    }
+
+    public function getSession_data() {
+        return $this->session_data;
+    }
+
+    public function setSession_data($session_data) {
+        $this->session_data = $session_data;
+    }
+
+    public function getExpires() {
+        return $this->expires;
+    }
+
+    public function setExpires($expires) {
+        $this->expires = $expires;
+    }
+
+        
     public function __construct() {
         
         $this->tableName = parent::PREFIX_TABLE.'_'.$this->tableName;
@@ -36,10 +60,72 @@ class SessionModel extends BaseModel{
 
     public function find($fields = null, $and = null, $where = null, $order = null, $limit = null, $asoc = TRUE) {
         
+           $query = "SELECT ";
+        if($fields!=null && is_array($fields)){
+            
+     
+            
+            $query.= implode(",", $fields)." ";
+            //echo $query;
+            
+        }else{
+            
+            $query .= "*";
+        }
+        $query .=" FROM ".$this->tableName;
+        
+        if($where!=null&& is_array($where)){
+            if(is_string($where['value'])) $where['value'] = "'".$where['value']."'";
+            $query.=" WHERE ".$where['key']." =".$where['value'];            
+        }
+        
+        if($and!=null&&  is_array($and)){
+            
+            foreach ($and as $andValue){
+                 if(is_string($andValue['value'])) $andValue['value'] = "'".$andValue['value']."'";
+                $query.=" AND ".$andValue['key']." ".$andValue['math'].$andValue['value'];
+            }
+            
+            
+        }
+        
+        if($order!=null){
+            $query.=" ORDER BY ".$order['key']." ".$order['sort'];
+        }
+        
+        if($limit!=null){
+            $query.=" LIMIT ".$limit['from'].' '.$limit['lenght'];
+        }
+        try {
+            //echo $query;
+            $stm = self::$dbo->prepare($query);
+            $stm->execute();
+            
+            if($asoc==TRUE) return $stm->fetchAll(PDO::FETCH_ASSOC);
+            else return $stm->fetchAll();
+            
+        }  catch (Exception $ex){
+            
+             throw  new Exception("Cant find ".$this->tableName." ".$ex->getMessage());
+        }
+        
+        
+        
     }
 
     public function save($returnLastID = FALSE) {
-        
+           try{
+             //print_r($this->tableName);
+
+             $query = "REPLACE ".$this->tableName."(session_id,session_data,expires) VALUE(?,?,?)";
+             $stm = self::$dbo->prepare($query);
+             $resulft = $stm->execute(array($this->session_data,$this->session_data,$this->expires));
+             if($returnID==TRUE) return $stm->lastInsertId(); 
+            
+        }catch(PDOException $ex){
+            
+            throw  new Exception("Cant save ".$this->tableName." ".$ex->getMessage());
+        }
         
         
     }
